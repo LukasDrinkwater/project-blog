d6579d2485650013d01dd7e7a80a5013fc13b47c
Lukas <lukasdrinkwater97@gmail.com>
2023-12-15 21:47:48 +0000

    changed param name

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -9,4 +9,7 @@ router.get("/", blogController.blog_list);
 // GET create blog
 router.get("/blog", blogController.blog_update_get);
 
+// GET specific blog
+router.get("/:blogId", blogController.blog_detail);
+
 module.exports = router;
