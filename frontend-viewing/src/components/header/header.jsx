535ac76d9311408dd27acf00a768b223a53e19b9
Lukas <lukasdrinkwater97@gmail.com>
2023-12-17 18:12:55 +0000

    added sign up login logout buttons navlinks

--- frontend-viewing/src/components/header/header.jsx
+++ frontend-viewing/src/components/header/header.jsx
@@ -8,6 +8,10 @@ function Header() {
       <NavLink to="/blogs"> All Blogs</NavLink>
       <NavLink to="/users"> All Users</NavLink>
       <NavLink to="/signup"> Sign Up</NavLink>
+      <NavLink to="/login"> Login</NavLink>
+      <form action="http://localhost:5173/logout" method="GET">
+        <button type="submit">Logout</button>
+      </form>
     </div>
   );
 }
