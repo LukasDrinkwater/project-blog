cda9f69919278d9d0dbfa1a2384df99a0f5d86ac
Lukas <lukasdrinkwater97@gmail.com>
2023-12-21 16:51:49 +0000

    trouble shooting and fixing passport login authorisation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var router = express.Router();
+const passport = require("passport");
 
 const loginController = require("../controllers/loginController");
 // const authenticate = require("../strategies/authentication");
+const login = require("../strategies/authentication");
 
 // POST signup form
 router.post("/signup", loginController.signup_attempt_post);
 
 // POST Login form
-router.post("/login", loginController.login_attempt_post);
+// router.post("/login", loginController.login_attempt_post);
 // alternate
-// router.post("/login", authenticate.authenticate_login);
+router.post("/login", passport.authenticate("local"), login);
 
 // POST logout request
 router.post("/logout", loginController.logout_post);
