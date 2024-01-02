e747fa9864730bc948b89285f12923b726b1b0d7
Lukas <lukasdrinkwater97@gmail.com>
2024-01-02 13:20:54 +0000

    start working on functionality to delete a comment

--- frontend-viewing/src/components/blog-components/blogDetail/singleComment.jsx
+++ frontend-viewing/src/components/blog-components/blogDetail/singleComment.jsx
@@ -1,6 +1,8 @@
 // Component for each individual comment
 
-function SingleComment({ text, user, createdAtFormatted, blog }) {
+function SingleComment({ text, user, createdAtFormatted, blog, _id }) {
+  const deleteCommentClick = (commentId) => {};
+
   return (
     <div className="commentCard">
       <p>{user.fullName}</p>
