39f5ac3de4b39d2638f63eda394942d0a4e350e4
Lukas <lukasdrinkwater97@gmail.com>
2024-01-03 15:49:46 +0000

    amended code to work with updated loggedIn state and ternary operators

--- frontend-viewing/src/components/blog-components/blogDetail/singleComment.jsx
+++ frontend-viewing/src/components/blog-components/blogDetail/singleComment.jsx
@@ -1,13 +1,48 @@
 // Component for each individual comment
 
-function SingleComment({ text, user, createdAtFormatted, blog, _id }) {
-  const deleteCommentClick = (commentId) => {};
+import { useState } from "react";
+
+function SingleComment({
+  update,
+  setUpdate,
+  text,
+  user,
+  createdAtFormatted,
+  blog,
+  _id,
+}) {
+  const [error, setError] = useState("");
+
+  const deleteCommentClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:3000/blogs/${blog}/comment/${_id}/delete`,
+        {
+          credentials: "include",
+          mode: "cors",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        setUpdate(!update);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error:", error.message);
+    }
+  };
 
   return (
     <div className="commentCard">
       <p>{user.fullName}</p>
       <p>{text}</p>
       <p>{createdAtFormatted}</p>
+      <button onClick={deleteCommentClick}>REMOVE</button>
+      <p>{error}</p>
     </div>
   );
 }
