441ed3712651ca5d3fee4163420be7f11806a3bb
Lukas <lukasdrinkwater97@gmail.com>
2024-01-07 15:44:53 +0000

    commented out use of navlink

--- frontend-editing/src/components/blog-components/blogDetail/blogDetail.jsx
+++ frontend-editing/src/components/blog-components/blogDetail/blogDetail.jsx
@@ -1,9 +1,15 @@
 // Component that renders the blog
+// import { NavLink } from "react-router-dom";
 
 function BlogDetail({ blog }) {
   const blogContent = blog.content;
+  // const blogId = blog._id;
+
   return (
     <div className="blogContainer">
+      {/* <span>
+        <NavLink to="edit"> Edit blog post.</NavLink>
+      </span> */}
       <p>{blogContent}</p>
     </div>
   );
