0ea695c16f49dd7ea7d2b42028992011bc614809
Lukas <lukasdrinkwater97@gmail.com>
2024-01-07 15:49:20 +0000

    added route for blog update PUT

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -7,12 +7,18 @@ const blogController = require("../controllers/blogController");
 router.get("/", blogController.blog_list);
 
 // GET create blog
-router.get("/blog", blogController.blog_update_get);
+// router.get("/blog", blogController.blog_update_get);
 
 // GET specific blog
 router.get("/:blogId", blogController.blog_detail);
 
 // // POST new comment to blog
 // router.post("/:blogId/comment", blogController.blog_add_comment_post);
+
+// GET specific blog to EDIT
+router.get("/:blogId/edit", blogController.blog_update_get);
+
+// POST update specific blog
+router.put("/blogId/edit", blogController.blog_update_put);
 
 module.exports = router;
