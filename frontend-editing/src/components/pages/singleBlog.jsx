16ff108688bae857a579d982b780224ce254ec07
Lukas <lukasdrinkwater97@gmail.com>
2024-01-07 15:46:58 +0000

    changed toggle edit button

--- frontend-editing/src/components/pages/singleBlog.jsx
+++ frontend-editing/src/components/pages/singleBlog.jsx
@@ -4,11 +4,15 @@ import BlogDetail from "../blog-components/blogDetail/blogDetail";
 import BlogHeader from "../blog-components//blogDetail/blogHeader";
 import BlogComments from "../blog-components/blogDetail/blogComments";
 import CommentForm from "../addComment/commentForm";
+
+import EditSingleBlog from "./editSingleBlog";
+
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function SingleBlog() {
   const [blog, setBlog] = useState(null);
+  const [enableEdit, setEnableEdit] = useState(false);
   //update triggers the useEffect to run after a comment has been added or deleted.
   const [update, setUpdate] = useState(false);
   const [error, setError] = useState("");
@@ -38,26 +42,51 @@ function SingleBlog() {
     fetchData();
   }, [update]);
 
+  const handleEditBlogClick = () => {
+    setEnableEdit(!enableEdit);
+  };
+
   return (
-    <div className="singleBlogContainer">
-      {error && <p>Error loading blog: {error}</p>}
-      {blog !== null ? (
-        <>
-          <BlogHeader {...blog} />
-          <BlogDetail {...blog} />
-          <BlogComments
-            comments={blog.comments}
-            update={update}
-            setUpdate={setUpdate}
-          />
-        </>
+    <>
+      {!enableEdit ? (
+        <div className="editBlogButton">
+          <button onClick={handleEditBlogClick}>Edit blog.</button>
+        </div>
       ) : (
-        <p>Loading blog...</p>
+        <div className="ViewEditedBlogButton">
+          <button onClick={handleEditBlogClick}>View blog.</button>
+        </div>
       )}
-      <div className="addCommentContainer">
-        <CommentForm {...blog} update={update} setUpdate={setUpdate} />
-      </div>
-    </div>
+      {!enableEdit ? (
+        <div className="singleBlogContainer">
+          {error && <p>Error loading blog: {error}</p>}
+          {blog !== null ? (
+            <>
+              <BlogHeader {...blog} />
+              <BlogDetail {...blog} />
+              <BlogComments
+                comments={blog.comments}
+                update={update}
+                setUpdate={setUpdate}
+              />
+            </>
+          ) : (
+            <p>Loading blog...</p>
+          )}
+          <div className="addCommentContainer">
+            <CommentForm {...blog} update={update} setUpdate={setUpdate} />
+          </div>
+        </div>
+      ) : (
+        <div className="editSingleBlogContainer">
+          <EditSingleBlog
+            {...blog}
+            enableEdit={enableEdit}
+            setEnableEdit={setEnableEdit}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
