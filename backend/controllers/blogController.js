7b5e737be65b11075c2e452dd6ff8303f3aa69f9
Lukas <lukasdrinkwater97@gmail.com>
2024-01-11 21:51:23 +0000

    got update post handler function working correctly

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -6,6 +6,9 @@ const User = require("../models/user");
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
+// Entities is to decode any html text issues
+const he = require("he");
+
 // GET all blogs
 exports.blog_list = asyncHandler(async (req, res, next) => {
   // console.log("get blogs", req.user);
@@ -20,7 +23,7 @@ exports.blog_list = asyncHandler(async (req, res, next) => {
 
 // GET specific blog
 exports.blog_detail = asyncHandler(async (req, res, next) => {
-  // console.log(req.user);
+  console.log("getting blog");
   const [blog, allComments] = await Promise.all([
     Blog.findById(req.params.blogId).populate("user").exec(),
     Comment.find({ blog: req.params.blogId })
@@ -114,29 +117,43 @@ exports.blog_update_get = asyncHandler(async (req, res, next) => {
 
 // POST request to update blog
 
-exports.blog_update_put = [
-  // body("blogTitle", "Blog title must not be blank.")
-  //   .trim()
-  //   .isLength({ min: 1 })
-  //   .escape(),
-  // body("blogContent", "Blog content must not be blank")
-  //   .trim()
-  //   .isLength({ min: 1 })
-  //   .escape(),
+exports.blog_update_post = [
+  body("blogTitle", "Blog title must not be blank.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("blogContent", "Blog content must not be blank")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
 
   asyncHandler(async (req, res, next) => {
+    console.log("request received");
+    // console.log(req.body.blogContent)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       res.status(400).json({ error });
     } else {
-      const { title, content } = req.body;
+      const title = req.body.blogTitle;
+
+      // const content = req.body.blogContent;
+      const content = he.decode(req.body.blogContent);
+      console.log(content);
 
       const updatedBlog = await Blog.findByIdAndUpdate(
         req.params.blogId,
         { $set: { title, content } },
         { new: true }
       );
+
+      res.status(200).json({ message: "Blog saved" });
     }
   }),
 ];
+
+exports.blog_update_test = asyncHandler(async (res, req, next) => {
+  console.log("in blog_update_test");
+
+  res.status(200).res.json({ message: "here" }).send();
+});
