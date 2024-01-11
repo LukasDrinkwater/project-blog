1ecb073e6a522ee50987028ae1fa25562295280f
Lukas <lukasdrinkwater97@gmail.com>
2024-01-11 21:27:46 +0000

    added condition render of NavLink and Link depending if its the BlogList or UsersBlogList

--- frontend-editing/src/components/blog-components/blogList/blogPreview.jsx
+++ frontend-editing/src/components/blog-components/blogList/blogPreview.jsx
@@ -1,16 +1,25 @@
 // Preview of the blog
-import { Link } from "react-router-dom";
+import { Link, useParams, NavLink } from "react-router-dom";
 
 function BlogPreview({ title, user, createdAtFormatted, _id }) {
-  // console.log("country", createdAtFormatted);
+  const userIdParam = useParams();
+
   return (
     <div className="blogPreview">
-      <Link to={`${_id}`}>
-        <h2>{title}</h2>
-      </Link>
+      {/* if its showing a blog preview on the All Users page user === iserIdParam.userId
+      from the URL so it needs to be a NavLink to link to the blog/blogId url */}
+      {user._id === userIdParam.userId ? (
+        <NavLink to={`/blogs/${_id}`}>
+          <h2>{title}</h2>
+        </NavLink>
+      ) : (
+        <Link to={`${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+      )}
 
       <div className="previewDetail">
-        <p>Author: {user.fullName}</p>
+        <p>User: {user.fullName}</p>
         <p>From: {user.country}</p>
         <p>Posted:{createdAtFormatted}</p>
       </div>
